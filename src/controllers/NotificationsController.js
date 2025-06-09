1fa532909b7ec3e64e4cd1ed09a328d82b5d2b66
Rafael Rocha <rafaelrocha@outlook.com>
2025-06-09 20:45:04 -0300

    Update postNotification to use request body for notifiedTo and originalRequest

--- src/controllers/NotificationsController.js
+++ src/controllers/NotificationsController.js
@@ -6,8 +6,11 @@ module.exports = {
         const notification = {
             id: uuidv4(),
             receivedAt: new Date(),
-            notifiedTo: ["example"],
-            ...request.body
+            notifiedTo: request.body?.tags?.sendTo,
+            originalRequest: {
+                method: request.method,
+                body: request.body
+            }
         };
         console.log(JSON.stringify(notification, null, 2));
         return response.status(201).json(notification);
